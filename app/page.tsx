711fb8bc352f87cd19fac4ca7658ba14f31c41c7
shametim <9932250+shametim@users.noreply.github.com>
2023-03-06 22:55:20 -0500

    oh yeah

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { auth, currentUser } from "@clerk/nextjs/app-beta";
 
-export async function Home() {
+export default async function Home() {
   const user = await currentUser();
 
   return <div className="flex justify-center">{user?.id}</div>;
